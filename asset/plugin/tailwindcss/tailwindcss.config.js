e38b395d485d1e7749c727e13362068af275225d
khaingmyozaw <kmyozaw.dev@gmail.com>
2025-07-03 23:21:51 +0630

    nav responsive

--- asset/plugin/tailwindcss/tailwindcss.config.js
+++ asset/plugin/tailwindcss/tailwindcss.config.js
@@ -2,9 +2,10 @@ tailwind.config = {
     theme: {
         extend: {
             colors: {
-                black: '#000000',
+                black: '#212529',
                 white: '#ffffff',
                 gray: '#ffffff',
+                red: '#D11D41',
             }
         }
     }
