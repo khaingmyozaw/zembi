81a0441bca1222542479c32d567da6fb1dd8c5be
khaingmyozaw <kmyozaw.dev@gmail.com>
2025-07-13 01:12:56 +0630

    about section and home page responsive

--- asset/js/animation.js
+++ asset/js/animation.js
@@ -1,7 +1,56 @@
 $(document).ready(function () {
-
     $('#menu-btn').on('click', function () {
         $(this).find('div').toggleClass('active');
     });
 
+    const cons = 'console.';
+
+    // Appending effect to the pre element
+    function typeEffect(text, className, callback, wrapper = '#codeview') {
+        let index = 0;
+        let span = $('<span>', {
+            class: className,
+        }).appendTo($(wrapper));
+        let cursor = $('<span>', {
+            class: 'cursor'
+        }).appendTo($(wrapper));
+
+        // Typing characters one by one
+        function startTyping() {
+            if (index < text.length) {
+                span.append(text.charAt(index));
+                index++;
+                // recurrsive this function according to text's length
+                setTimeout(startTyping, 100);
+            } else {
+                if (callback) {
+                    cursor.remove();
+                    callback();
+                }
+            }
+        }
+
+        startTyping(span, text, index);
+
+    }
+    typeEffect('console.', 'text-teal-300', () => {
+        typeEffect('log(', 'text-blue-400', () => {
+            typeEffect('"Hello, world!"', 'text-green-400', () => {
+                typeEffect(')', 'text-blue-400', () => {
+                    typeEffect(';', '', () => {
+                        $('#codeview').append($('<br><br>'))
+                        typeEffect('const ', 'text-teal-300', () => {
+                            typeEffect('name ', 'text-yellow-300', () => {
+                                typeEffect('= ', '', () => {
+                                    typeEffect('"Khaing Myo Zaw"', 'text-green-400', () => {
+                                        typeEffect(';', '');
+                                    });
+                                });
+                            });
+                        });
+                    });
+                });
+            });
+        });
+    });
 })
