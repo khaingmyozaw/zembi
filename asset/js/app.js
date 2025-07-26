185a103f4853f83423243a16ebc50374189d7b44
khaingmyozaw <kmyozaw.dev@gmail.com>
2025-07-26 18:39:27 +0630

    theme button added

--- asset/js/app.js
+++ asset/js/app.js
@@ -1,7 +1,15 @@
-let isDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches; // Get OS's theme
+// let isDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches; // Get OS's theme
 
-if (isDarkMode) {
-    document.documentElement.classList.add('dark');
+// if (isDarkMode) {
+//     document.documentElement.classList.add('dark');
+// }
+function toggleTheme(btn) {
+    $('html').toggleClass('dark');
+    if ($('html').hasClass('dark')) {
+        btn.find('img').attr('src', './asset/images/svgs/moon.svg')
+    }else {
+        btn.find('img').attr('src', './asset/images/svgs/sun.svg')
+    }
 }
 
 function copyEmail(element, data) {
