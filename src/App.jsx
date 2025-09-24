b365854957cd0d013a56689411b0a8db63f8bca3
khaingmyozaw <kmyozaw.dev@gmail.com>
2025-09-24 23:38:06 +0630

    setting up

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Nav from "./components/Nav"
+
+function App() {
+  return (
+    <>
+      <Nav />
+      <main className="bg-gray-300 dark:bg-[#030713]">
+
+      </main>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Nav.jsx
@@ -0,0 +1,8 @@
+export default function Nav() {
+    return (
+        
+        <nav className="bg-red-500 p-3">
+            Hello HELLO
+        </nav>
+    )
+}
